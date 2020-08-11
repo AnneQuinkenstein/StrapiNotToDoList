26bbfa85cd1d67cad1eb71af1ecb9f553dcf9d6d
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-08-11 19:44:19 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b543c86d15be5fd4e112c2d798fc644c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '48181103-d513-4218-a763-0e9909e6790b'
+};
